dc56d124fd3685e67037a5553e0548a0287c5e6c
Hylke Stapersma <hylke.stapersma@gmail.com>
2013-01-02 19:53:08 +0100

    Add first parts for functional testing

---
+++ TDDTrainingApplicationCC/src/test/java/functional/nl/codecentric/training/tdd/web/Application.java
@@ -0,0 +1,14 @@
+package functional.nl.codecentric.training.tdd.web;
+
+/**
+ * User: Hylke Stapersma
+ * E-mail:[ hylke.stapersma@codecentric.nl]
+ * Date: 02-01-13
+ * Time: 19:33
+ */
+public class Application {
+    public class TriangleCalculation{
+        public class NonNumericInput{};
+    }
+
+}
---
+++ TDDTrainingApplicationCC/src/test/java/functional/nl/codecentric/training/tdd/web/page/TriangleCalculationPage.java
@@ -0,0 +1,10 @@
+package functional.nl.codecentric.training.tdd.web.page;
+
+/**
+ * User: Hylke Stapersma
+ * E-mail:[ hylke.stapersma@codecentric.nl]
+ * Date: 02-01-13
+ * Time: 19:42
+ */
+public class TriangleCalculationPage {
+}
